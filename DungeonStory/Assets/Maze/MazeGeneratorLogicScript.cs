1599f0a51283509374d001ac9c4769042c59dfca
88jin@mail.ru <88jin@mail.ru>
2020-11-07 14:06:55 +0300

    One more init commit. For now with good .gitignore file

---
+++ DungeonStory/Assets/Maze/MazeGeneratorLogicScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeGeneratorLogicScript : MonoBehaviour
+{
+    //private GameObject wall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetCompo
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
