28017e78b3284c67d5ba1867f2b9b913754c05c2
88jin@mail.ru <88jin@mail.ru>
2020-11-13 12:30:00 +0300

    Use prefab

--- DungeonStory/Assets/Maze/MazeGeneratorLogicScript.cs
+++ DungeonStory/Assets/Maze/MazeGeneratorLogicScript.cs
@@ -8,11 +8,14 @@ using UnityEngine;
 public class MazeGeneratorLogicScript : MonoBehaviour
 {
     public GameObject wallBrickTemplate;
+    public GameObject coinTemplate;
 
     public int width;
     public int heigth;
     public double cointChance;
     public int fontaineCount;
+
+    public float blockSize;
 
     // Start is called before the first frame update
     void Start()
@@ -20,35 +23,60 @@ public class MazeGeneratorLogicScript : MonoBehaviour
         var mazeBuilder = new MazeBuilder();
         var maze = mazeBuilder.Generate(width, heigth, cointChance, fontaineCount);
 
-        foreach (var cell in maze.CellsWithPlayer.OfType<Wall>())
-        {
-            var wallGameObject = Instantiate(wallBrickTemplate);
-            wallGameObject.GetComponent<Transform>().position
-                = new Vector3(cell.X, 1, cell.Z);
-        }
-
-        for (int i = -1; i < width + 1; i++)
-        {
-            var wallGameObjectBottom = Instantiate(wallBrickTemplate);
-            wallGameObjectBottom.GetComponent<Transform>().position = new Vector3(i, 1, -1);
-
-            var wallGameObjectTop = Instantiate(wallBrickTemplate);
-            wallGameObjectTop.GetComponent<Transform>().position = new Vector3(i, 1, heigth);
-        }
-
-        for (int i = 0; i < heigth; i++)
-        {
-            var wallGameObjectLeft= Instantiate(wallBrickTemplate);
-            wallGameObjectLeft.GetComponent<Transform>().position = new Vector3(-1, 1, i);
-
-            var wallGameObjectRight= Instantiate(wallBrickTemplate);
-            wallGameObjectRight.GetComponent<Transform>().position = new Vector3(width, 1, i);
-        }
+        DrawMaze(maze);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private void DrawMaze(MazeLevelBusinessObject maze)
+    {
+        foreach (var cell in maze.CellsWithPlayer)
+        {
+            GameObject gameObject = null;
+            if (cell is Wall)
+            {
+                gameObject = Instantiate(wallBrickTemplate);
+            }
+            if (cell is Coin)
+            {
+                gameObject = Instantiate(coinTemplate);
+            }
+
+            TrasformCell(gameObject, cell.X, cell.Z);
+        }
+
+        //foreach (var cell in maze.CellsWithPlayer.OfType<Wall>())
+        //{
+        //    var wallGameObject = Instantiate(wallBrickTemplate);
+        //    wallGameObject.GetComponent<Transform>().position
+        //        = new Vector3(cell.X, 1, cell.Z);
+        //}
+
+        //Draw border wall
+        for (int i = -1; i < width + 1; i++)
+        {
+            TrasformCell(Instantiate(wallBrickTemplate), i, -1);
+            TrasformCell(Instantiate(wallBrickTemplate), i, heigth);
+        }
+
+        for (int i = 0; i < heigth; i++)
+        {
+            TrasformCell(Instantiate(wallBrickTemplate), -1, i);
+            TrasformCell(Instantiate(wallBrickTemplate), width, i);
+        }
+    }
+
+    private void TrasformCell(GameObject gameObject, int x, int z)
+    {
+        if (gameObject != null)
+        {
+            var cellTransform = gameObject.GetComponent<Transform>();
+            cellTransform.position = new Vector3(x * blockSize, 1, z * blockSize);
+            cellTransform.localScale = new Vector3(blockSize, 1, blockSize);
+        }
     }
 }
